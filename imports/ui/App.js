c451d81d38342f75d3db48844a960517dd2aca2c
Caleb <cperry@fancherblack.com>
2019-02-04 15:03:34 -0500

    Added timer

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Helmet } from "react-helmet";
+import  Timer  from 'react-compound-timer';
 import { Tasks } from '../api/tasks.js';
 import Task from './Task.js';
 
@@ -55,6 +56,35 @@ class App extends Component {
           <header>
             <h1>Todo List ({this.props.incompleteCount})</h1>
 
+            <Timer
+              initialTime={60000 * 30}
+              startImmediately={false}
+              direction="backward"
+              checkpoints={[
+                {
+                  time: 0,
+                  callback: () => alert("Time's up!"),
+                },
+              ]}
+            >
+                {({ start, resume, pause, stop, reset, timerState }) => (
+                    <React.Fragment>
+                        <div>
+                            <Timer.Minutes /> minutes
+                            <Timer.Seconds /> seconds
+                        </div>
+                        <div>{timerState}</div>
+                        <br />
+                        <div>
+                            <button onClick={start}>Start</button>
+                            <button onClick={pause}>Pause</button>
+                            <button onClick={resume}>Resume</button>
+                            <button onClick={stop}>Stop</button>
+                            <button onClick={reset}>Reset</button>
+                        </div>
+                    </React.Fragment>
+                )}
+            </Timer>
             <label className="hide-completed">
               <input type="checkbox" readOnly checked={this.state.hideCompleted} onClick={this.toggleHideCompleted.bind(this)}/>
               Hide Completed Tasks
